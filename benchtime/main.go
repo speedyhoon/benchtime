9aa49a0c43d2db9be9d72d39bca9084ba374131f
sh <16303791+speedyhoon@users.noreply.github.com>
2025-04-17 09:53:34 +1000

    Move cleanPath() into the utl package.

--- benchtime/main.go
+++ benchtime/main.go
@@ -4,13 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/speedyhoon/benchtime"
+	"github.com/speedyhoon/utl"
 	"github.com/spf13/pflag"
 	"log"
 	"os"
-	"os/user"
-	"path/filepath"
-	"runtime"
-	"strings"
 )
 
 func main() {
@@ -67,7 +64,7 @@ func main() {
 
 	for _, path := range paths {
 		var err error
-		path, err = cleanPath(path)
+		path, err = utl.CleanPath(path)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -82,18 +79,4 @@ func main() {
 
 		fmt.Println(benchtime.Calculate(src, *decimalsQty, sortColumn))
 	}
-}
-
-func cleanPath(filePath string) (_ string, err error) {
-	const homeDir = "~"
-	if strings.HasPrefix(filePath, homeDir) && (runtime.GOOS == "linux" || runtime.GOOS == "darwin") {
-		var usr *user.User
-		usr, err = user.Current()
-		if err != nil {
-			return
-		}
-		filePath = strings.Replace(filePath, homeDir, usr.HomeDir, 1)
-	}
-
-	return filepath.Abs(filePath)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/speedyhoon/benchtime
 go 1.21.4
 
 require (
+	github.com/speedyhoon/utl v0.0.0-20250416234816-20f7a523763c
 	github.com/spf13/pflag v1.0.6
-	github.com/stretchr/testify v1.9.0
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
